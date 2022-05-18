933b356854c04aff95d19bb5830a82cf01e7b894
FranciscoLozCoding <z1842358@students.niu.edu>
2022-05-18 12:07:31 -0500

    added code to save images in a image folder

---
+++ TakePictures.py
@@ -0,0 +1,35 @@
+import os
+import cv2 #video streaming
+import datetime
+
+camIP = '10.42.0.103'
+username = 'admin'
+password = 'admin'
+streamURL = 'rtsp://' + username + ':' + password + '@' + camIP + ':554/cam/realmonitor?channel=1&subtype=1'
+
+cam = cv2.VideoCapture(streamURL)
+
+img_date = datetime.datetime.now()
+path = '/Images'
+
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        print("failed to grab frame")
+        break
+    cv2.imshow("Image", frame)
+
+    img_name="frame_{}.png".format(img_date)
+    img_path=os.path.join(path,img_name)
+    cv2.imwrite(img_path, frame)
+    print("{} written!".format(img_name))
+
+    k = cv2.waitKey(1)
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+
+cam.release()
+
+cv2.destroyAllWindows()
