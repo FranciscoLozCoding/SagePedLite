56c240ef22a36a84a340ea1e2c2590fe865ab8aa
Francisco Lozano <z1842358@students.niu.edu>
2022-05-27 10:50:08 -0500

    changed the model to pednet

--- my-detection.py
+++ my-detection.py
@@ -1,6 +1,7 @@
 import jetson.utils
 from TrackPerson import Person
 import numpy as np
+import time
 
 #Camera Variables
 camIP = '10.42.0.78'
@@ -9,12 +10,15 @@ password = 'admin'
 streamURL = 'rtsp://' + username + ':' + password + '@' + camIP + ':554/cam/realmonitor?channel=1&subtype=1'
 
 #set up camera and display
-camera = jetson.utils.videoSource(streamURL)
+# camera = jetson.utils.videoSource(streamURL) # Armcrest camera
+camera = jetson.utils.videoSource("file:///home/flozano/Workspace/SagePedLite/example.mp4") # example video
 display = jetson.utils.videoOutput("file://my_video.mp4")
 
-#Has to be after videoOutput for the X11 window to create
-import jetson.inference 
-net = jetson.inference.detectNet("ssd-mobilenet-v2",threshold=0.5) #load model
+
+# The model does better at detecting objects when it has a leveled view of the objects
+# For example if the camera is looking down at the objects the model doesn't do well
+import jetson.inference  #Has to be after videoOutput for the X11 window to create
+net = jetson.inference.detectNet("pednet",threshold=0.5) #load model
 
 #Make sure the GPU is done work
 jetson.utils.cudaDeviceSynchronize()
@@ -26,7 +30,8 @@ font = jetson.utils.cudaFont( size=32 )
 from centroidtracker import CentroidTracker
 ct = CentroidTracker()
 trackableObjects = {}
-people = []
+
+time.sleep(2.0)
 
 while True:
 
@@ -45,31 +50,38 @@ while True:
     # loop over the tracked objects
     for (objectID,centroid) in objects.items():
 
-        # check to see if a person exists for the currect object ID
-        to = trackableObjects.get(objectID, None)
+        # # check to see if a person exists for the currect object ID
+        # to = trackableObjects.get(objectID, None)
 
-        # if there is no existing person, create one
-        if to is None:
-            to = Person(objectID, centroid)
-            people.append(to)
-        #log the direction and tracjectory of the person walking
-        elif to.lastPoint:
-            # check if the direction of the object has been set, if
-            # not, calculate it, and set it
-            if to.direction is None:
-                y = [c[0] for c in to.centroids]
-                direction = centroid[0] - np.mean(y)
-                to.direction = direction
-        # otherwise collect new cord
-        else:
-            for x in people:
-                if x.objectID == objectID:
-                    x.addCord(centroid)
+        # # if there is no existing person, create one
+        # if to is None:
+        #     to = Person(objectID, centroid)
+
+        #     #store the trackable object in our dictionary
+        #     trackableObjects[objectID] = to
+
+        # #log the direction and tracjectory of the person walking
+        # elif to.lastPoint:
+        #     # check if the direction of the object has been set, if
+        #     # not, calculate it, and set it
+        #     if to.direction is None:
+        #         y = [c[0] for c in to.centroids]
+        #         direction = centroid[0] - np.mean(y)
+        #         to.direction = direction
+        # # otherwise collect new cord
+        # else:
+        #     to.addCord(centroid)
 
         #draw both the ID of the object and the centroid of the object on the output frame
         text = "ID {}".format(objectID)
         font.OverlayText(img,img.width,img.height,text,centroid[0] - 10,centroid[1] + 10,font.White)
         jetson.utils.cudaDrawCircle(img,centroid,4,(0,255,0))
+    
+    # #loop through the tracked people to draw their tracjectory lines
+    # for key in trackableObjects:
+    #     i = 0
+    #     while i <= len(trackableObjects[key].centroids) - 2:
+    #         jetson.utils.cudaDrawLine(img,trackableObjects[key].centroids[i],trackableObjects[key].centroids[i + 1],(0,255,0),10)
 
     display.Render(img) #show image
     display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS())) #update window title
