74f1c2f6e6adeae2f26314a9eb54b9320e841f1a
FranciscoLozCoding <z1842358@students.niu.edu>
2022-06-23 10:57:38 -0500

    fix loop interruption bug

--- TakePictures.py
+++ TakePictures.py
@@ -16,55 +16,50 @@ cam = cv2.VideoCapture(streamURL)
 #path to image folder
 Image_Base_Path = os.path.join(os.getcwd(),"Images")
 
-print("\n Type 'stop' to stop taking photos")
+print("\n Press 'ctrl + c' to stop taking photos")
 
 print("\n Photos are being captured... \n")
 
-#Capture images and show stream until esc key is pressed
-while True:
+try:
+    #Capture images until ctrl + c is pressed
+    while True:
 
-    end = input()
+        #show stream
+        ret, frame = cam.read()
+        if not ret:
+            print("failed to grab frame")
+            break
 
-    #show stream
-    ret, frame = cam.read()
-    if not ret:
-        print("failed to grab frame")
-        break
+        #capture images with name based on datetime and organized by date
+        img_dateTime = datetime.now()
+        month = "%02d" % (img_dateTime.month)
+        day = "%02d" % (img_dateTime.day)
+        year = "%04d" % (img_dateTime.year)
 
-    #capture images with name based on datetime and organized by date
-    img_dateTime = datetime.now()
-    month = "%02d" % (img_dateTime.month)
-    day = "%02d" % (img_dateTime.day)
-    year = "%04d" % (img_dateTime.year)
+        #create directory if it doesnt exist
+        if not os.path.isdir(Image_Base_Path + "/" + year): #create yyyy/mm/dd folder if yyyy folder doesnt exist
+            os.makedirs(Image_Base_Path + "/" + year + "/" + month + "/" + day)
+        elif not os.path.isdir(Image_Base_Path + "/" + year + "/" + month): #create yyyy/mm/dd folder if yyyy/mm folder doesnt exist
+            os.makedirs(Image_Base_Path + "/" + year + "/" + month + "/" + day)
+        elif not os.path.isdir(Image_Base_Path + "/" + year + "/" + month + "/" + day): #create yyyy/mm/dd folder if yyyy/mm/dd folder doesnt exist
+            os.makedirs(Image_Base_Path + "/" + year + "/" + month + "/" + day)
 
-    #create directory if it doesnt exist
-    if not os.path.isdir(Image_Base_Path + "/" + year): #create yyyy/mm/dd folder if yyyy folder doesnt exist
-        os.makedirs(Image_Base_Path + "/" + year + "/" + month + "/" + day)
-    elif not os.path.isdir(Image_Base_Path + "/" + year + "/" + month): #create yyyy/mm/dd folder if yyyy/mm folder doesnt exist
-        os.makedirs(Image_Base_Path + "/" + year + "/" + month + "/" + day)
-    elif not os.path.isdir(Image_Base_Path + "/" + year + "/" + month + "/" + day): #create yyyy/mm/dd folder if yyyy/mm/dd folder doesnt exist
-        os.makedirs(Image_Base_Path + "/" + year + "/" + month + "/" + day)
+        #path to the image taken
+        Image_Path = os.path.join(Image_Base_Path,year,month,day)
+        
+        #Create image
+        img_name="{}.jpg".format(img_dateTime)
+        img_path=os.path.join(Image_Path,img_name)
+        cv2.imwrite(img_path, frame)
 
-    #path to the image taken
-    Image_Path = os.path.join(Image_Base_Path,year,month,day)
-    
-    #Create image
-    img_name="{}.jpg".format(img_dateTime)
-    img_path=os.path.join(Image_Path,img_name)
-    cv2.imwrite(img_path, frame)
+        time.sleep(seconds_between_frames)
 
-    if end.lower() == 'stop':
-        # ESC pressed
-        print("Escape initiated, closing...")
-        break
-    else:
-        print("ERROR: To stop taking photos type 'stop'")
+except KeyboardInterrupt:
 
+    print("\n \n Escape initiated, closing camera...\n \n")
 
-    time.sleep(seconds_between_frames)
+    #closes camera
+    cam.release()
 
-#closes camera
-cam.release()
-
-#close window
-cv2.destroyAllWindows()
+    #close window
+    cv2.destroyAllWindows()
