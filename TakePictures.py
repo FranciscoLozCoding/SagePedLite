a5b1041928828777689e64bc9e8be17677ad310a
FranciscoLozCoding <z1842358@students.niu.edu>
2022-06-23 09:48:20 -0500

    added a method to stop taking photos

--- TakePictures.py
+++ TakePictures.py
@@ -8,22 +8,28 @@ seconds_between_frames = 1.0 #seconds between the frames captured to jpg
 
 #Camera Variables
 camIP = '10.42.0.104'
-username = 'admin'
-password = 'admin'
+username = 'admin' #default: admin
+password = 'Whynotnano' #default: admin
 streamURL = 'rtsp://' + username + ':' + password + '@' + camIP + ':554/cam/realmonitor?channel=1&subtype=1'
 cam = cv2.VideoCapture(streamURL)
 
 #path to image folder
 Image_Base_Path = os.path.join(os.getcwd(),"Images")
 
+print("\n Type 'stop' to stop taking photos")
+
+print("\n Photos are being captured... \n")
+
 #Capture images and show stream until esc key is pressed
 while True:
+
+    end = input()
+
     #show stream
     ret, frame = cam.read()
     if not ret:
         print("failed to grab frame")
         break
-    cv2.imshow("Image", frame)
 
     #capture images with name based on datetime and organized by date
     img_dateTime = datetime.now()
@@ -47,12 +53,13 @@ while True:
     img_path=os.path.join(Image_Path,img_name)
     cv2.imwrite(img_path, frame)
 
-    #get key pressed
-    k = cv2.waitKey(1)
-    if k%256 == 27:
+    if end.lower() == 'stop':
         # ESC pressed
-        print("Escape hit, closing...")
+        print("Escape initiated, closing...")
         break
+    else:
+        print("ERROR: To stop taking photos type 'stop'")
+
 
     time.sleep(seconds_between_frames)
 
